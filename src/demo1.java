dd6bf400baf5ca82294b0785d22806824e28d03d
likaixin <lkai04538@gmail.com>
2023-09-09 20:16:25 +0800

    hot first

--- src/demo1.java
+++ src/demo1.java
@@ -2,5 +2,6 @@ public class demo1 {
     public static void main(String[] args) {
         System.out.println("lll");
         System.out.println("2222");
+        System.out.println("hot fix");
     }
 }
